8f246a6d5d7d343e3a84541aad42077fd9aaf252
bhupesh-sf <bhupesh.gupta@sourcefuse.com>
2024-05-03 17:27:18 +0530

    Fix division by zero bug in Calculator class

--- calc.js
+++ calc.js
@@ -19,7 +19,9 @@ class Calculator {
     }
 
     divide(number) {
-
+        if (number === 0) {
+            throw new Error("Cannot divide by zero");
+        }
         this.result /= number;
         return this;
     }
