4059da4973fb51d801eb0b0546b022aeb0447fb4
bhupesh-sf <bhupesh.gupta@sourcefuse.com>
2024-05-03 17:27:00 +0530

    Fix divide method in Calculator class

--- calc.js
+++ calc.js
@@ -19,9 +19,7 @@ class Calculator {
     }
 
     divide(number) {
-        if (number === 0) {
-            throw new Error("Cannot divide by zero");
-        }
+
         this.result /= number;
         return this;
     }
